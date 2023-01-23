9ea9d1a752e55cbb475eabcb686e166b9a88cea2
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2023-01-22 18:51:02 -0800

    Moved the debug rendering of LDtk levels to pig_render_funcs_extra.cpp

--- pig/pig_render_funcs_extra.cpp
+++ pig/pig_render_funcs_extra.cpp
@@ -145,3 +145,90 @@ v2 RcDrawSteamFriendCard(u64 friendId, v2 position, v2 minSize, v2 maxSize, r32 
 
 #endif //STEAM_BUILD
 
+// +--------------------------------------------------------------+
+// |                   LDtk Level Debug Render                    |
+// +--------------------------------------------------------------+
+void RcDrawLdtkLevel(LdtkProject_t* project, LdtkLevel_t* level, v2 position, r32 scale, bool drawTiles = true, bool drawEntities = true)
+{
+	rec levelBounds = NewRec(position, ToVec2(level->bounds.size) * scale);
+	RcDrawRectangle(levelBounds, ColorTransparent(Black, 0.5f));
+	
+	// +==============================+
+	// | Render Tiles Layer Instances |
+	// +==============================+
+	VarArrayLoop(&level->layerInstances, lIndex)
+	{
+		VarArrayLoopGet(LdtkLayerInstance_t, layerInstance, &level->layerInstances, lIndex);
+		if (layerInstance->type == LdtkLayerType_Tiles)
+		{
+			NotNull(layerInstance->tiles.tilesetDefPntr);
+			LdtkTilesetDef_t* tilesetDef = layerInstance->tiles.tilesetDefPntr;
+			v2i gridSize = layerInstance->tiles.gridSize;
+			v2i tileSize = layerInstance->tiles.tileSize;
+			
+			if (lIndex == 0)
+			{
+				for (i32 tileY = 0; tileY < gridSize.height; tileY++)
+				{
+					for (i32 tileX = 0; tileX < gridSize.width; tileX++)
+					{
+						if (((tileX + tileY) % 2) == 0)
+						{
+							rec tileBounds = NewRec(0, 0, ToVec2(tileSize) * scale);
+							tileBounds.topLeft = levelBounds.topLeft + Vec2Multiply(tileBounds.size, NewVec2((r32)tileX, (r32)tileY));
+							RcDrawRectangle(tileBounds, ColorTransparent(White, 0.5f));
+						}
+					}
+				}
+			}
+			
+			VarArrayLoop(&layerInstance->tiles.tileInstances, iIndex)
+			{
+				VarArrayLoopGet(LdtkTileInstance_t, tileInstance, &layerInstance->tiles.tileInstances, iIndex);
+				NotNull(tileInstance->tilePntr);
+				LdtkTilesetTile_t* tileDef = tileInstance->tilePntr;
+				NotNull(tilesetDef->spriteSheetPntr);
+				AccessResource(tilesetDef->spriteSheetPntr);
+				v2i frame = NewVec2i((i32)(tileDef->id % tilesetDef->gridSize.width), (i32)(tileDef->id / tilesetDef->gridSize.width));
+				rec tileBounds = NewRec(levelBounds.topLeft + (ToVec2(tileInstance->position) * scale), ToVec2(tileSize) * scale);
+				RcBindSpriteSheet(tilesetDef->spriteSheetPntr);
+				RcDrawSheetFrame(frame, tileBounds, White);
+			}
+		}
+	}
+	
+	// +==============================+
+	// |   Render Entity Instances    |
+	// +==============================+
+	VarArrayLoop(&level->layerInstances, lIndex)
+	{
+		VarArrayLoopGet(LdtkLayerInstance_t, layerInstance, &level->layerInstances, lIndex);
+		if (layerInstance->type == LdtkLayerType_Entities)
+		{
+			VarArrayLoop(&layerInstance->entities.entityInstances, eIndex)
+			{
+				VarArrayLoopGet(LdtkEntityInstance_t, entityInstance, &layerInstance->entities.entityInstances, eIndex);
+				NotNull(entityInstance->entityDefPntr);
+				LdtkEntityDef_t* entityDef = entityInstance->entityDefPntr;
+				rec entityBounds = RecScale(ToRec(entityInstance->bounds), scale);
+				entityBounds.topLeft += levelBounds.topLeft;
+				if (entityDef->usesTileIcon)
+				{
+					NotNull2(entityDef->iconTilesetPntr, entityDef->iconTilePntr);
+					LdtkTilesetDef_t* tilesetDef = entityDef->iconTilesetPntr;
+					LdtkTilesetTile_t* tileDef = entityDef->iconTilePntr;
+					NotNull(tilesetDef->spriteSheetPntr);
+					AccessResource(tilesetDef->spriteSheetPntr);
+					v2i frame = NewVec2i((i32)(tileDef->id % tilesetDef->gridSize.width), (i32)(tileDef->id / tilesetDef->gridSize.width));
+					RcBindSpriteSheet(tilesetDef->spriteSheetPntr);
+					RcDrawSheetFrame(frame, entityBounds, White);
+				}
+				else
+				{
+					RcDrawRectangle(entityBounds, MonokaiPurple);
+				}
+			}
+		}
+	}
+}
+
