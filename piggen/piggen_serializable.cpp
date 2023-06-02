d38cd60ace4dd5f51f47e058da1f3c62fdb7f4ba
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2023-06-01 19:25:04 -0700

    Moved PigGen code from this repository to it's own repository where it's expected to be put in the lib/include/piggen folder in any project

--- piggen/piggen_serializable.cpp
+++
@@ -1,9 +0,0 @@
-/*
-File:   piggen_serializable.cpp
-Author: Taylor Robbins
-Date:   05\09\2023
-Description: 
-	** Serializable structs are one of the main things that piggen supports generating code for.
-	** The proper input format can be seen in piggen_examples.h
-*/
-
